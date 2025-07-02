0cc49f8f33f78283a62dbd7364a86c50d2c66c3b
Yu Nilar Thinn <yunilarthinn97@gmail.com>
2025-07-03 01:06:46 +0630

    feat: Qr feature completed.

--- frontend/src/pages/CheckQR.tsx
+++ frontend/src/pages/CheckQR.tsx
@@ -1,5 +1,11 @@
+import QrUpload from "@/components/QR/QrUpload";
+
 const CheckQR = () => {
-  return <div>CheckQR</div>;
+      return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <QrUpload />
+    </div>
+  );
 };
 
 export default CheckQR;
