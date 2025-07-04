50d8cb5c2aacea0f5ad9988d53dc848627392d12
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2025-07-05 02:10:35 +0630

    feat: added sample carousel slide to home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,15 @@
+import { CarouselSlide } from "@/components/Home/CarouselSlide";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      {/* Carousel Slide */}
+      <CarouselSlide />
+      {/* Event List */}
+      {/* Total Details */}
+      {/* Venue List */}
+    </div>
+  );
 };
 
 export default Home;
