b7d131a3906a37655228977e71f84031f64478bc
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2025-07-05 15:45:00 +0630

    feat: added home page event section

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import { CarouselSlide } from "@/components/Home/CarouselSlide";
+import HomeEventList from "@/components/Home/HomeEventList";
 
 const Home = () => {
   return (
     <div>
       {/* Carousel Slide */}
-      <CarouselSlide />
+      <div className="mb-20">
+        <CarouselSlide />
+      </div>
       {/* Event List */}
+      <HomeEventList />
       {/* Total Details */}
       {/* Venue List */}
     </div>
